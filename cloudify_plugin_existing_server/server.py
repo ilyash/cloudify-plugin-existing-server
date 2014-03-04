2afc5cfc2cc735021907b44286880a4aa996828a
Ilya Sher <ilya.sher@coding-knight.com>
2014-03-04 14:47:10 +0200

    fix

--- cloudify_plugin_existing_server/server.py
+++ cloudify_plugin_existing_server/server.py
@@ -3,7 +3,7 @@ from cloudify.manager import get_node_state, update_node_state
 
 @operation
 def start(ctx, **kwargs):
-    node_state = get_node_state(node_id)
+    node_state = get_node_state(ctx.node_id)
     node_state['management_ip'] = ctx.properties['ip']
     update_node_state(node_state)
     ctx.set_started()
