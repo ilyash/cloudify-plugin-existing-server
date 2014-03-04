da57f794294e2ba2a1c6eff4231788fd4a731bdb
Ilya Sher <ilya.sher@coding-knight.com>
2014-03-04 18:11:37 +0200

    add cloudify_plugin_existing_server.server.get_state()

--- cloudify_plugin_existing_server/server.py
+++ cloudify_plugin_existing_server/server.py
@@ -6,7 +6,12 @@ def start(ctx, **kwargs):
     node_state = get_node_state(ctx.node_id)
     node_state['management_ip'] = ctx.properties['ip']
     update_node_state(node_state)
+    ctx['state'] = True
 
 @operation
 def stop(ctx, **kwargs):
-    pass
+    ctx['state'] = False
+
+@operation
+def get_state(ctx, **kwargs):
+    return ctx.runtime_properties['state']
