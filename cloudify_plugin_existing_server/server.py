c1fe4b9b6972fce76ed15400514666382411307d
Ilya Sher <ilya.sher@coding-knight.com>
2014-03-04 17:56:34 +0200

    remove ctx.set_started() / ctx.set_stopped()

--- cloudify_plugin_existing_server/server.py
+++ cloudify_plugin_existing_server/server.py
@@ -6,8 +6,7 @@ def start(ctx, **kwargs):
     node_state = get_node_state(ctx.node_id)
     node_state['management_ip'] = ctx.properties['ip']
     update_node_state(node_state)
-    ctx.set_started()
 
 @operation
 def stop(ctx, **kwargs):
-    ctx.set_stopped()
+    pass
